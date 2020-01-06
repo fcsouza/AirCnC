3d83e6ca626be4b48127a52d0169b482afdcc7b2
fcsouza <fcsouza18@hotmail.com>
2020-01-05 21:13:41 -0300

    inicio das rotas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+//GET, POST, PUT, DELETE
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisação( para criação, edição)
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
