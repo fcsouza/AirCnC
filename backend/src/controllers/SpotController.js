26e6a097980486d58e6dc5c738a42f44f82a7072
fcsouza <fcsouza18@hotmail.com>
2020-01-07 09:06:35 -0300

    Finazalição do SpotController

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,7 +1,27 @@
-module.exports = {
+const User = require('../models/User');
+const Spot = require('../models/Spot');
+
+
+module.exports = { 
     async store(req, res){
-        console.log(req.body);
-        console.log(req.file);
-        return res.json({ok : true});
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        const { user_id } = req.headers;
+
+        const user = await User.findById(user_id);
+
+        if (!user){
+            return res.status(400).json({ erro : "User does not exists" })
+        }
+
+        const spot = await Spot.create({
+            user : user_id,
+            thumbnail : filename,
+            company,
+            techs : techs.split(',').map( tech => tech.trim()),
+            price
+        })
+
+        return res.json(spot);
     }
 };
--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -11,4 +11,4 @@ const SpotSchema = new mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model('Spot', UserSchema);
+module.exports = mongoose.model('Spot', SpotSchema);
