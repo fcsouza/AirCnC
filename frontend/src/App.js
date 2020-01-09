f3b4f16839cd8affd2f875ef15debf1ccb5b6064
fcsouza <fcsouza18@hotmail.com>
2020-01-09 10:11:14 -0300

    Tela de form e css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="container">
+      <img src={logo} alt="AirCnC"></img>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor email"
+            />
+
+            <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
     );
 }
 
