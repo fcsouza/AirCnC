f2ecbe78bd665c6a583ebb92495a6ecab6b76387
fcsouza <fcsouza18@hotmail.com>
2020-01-12 23:12:37 -0300

    configuração do dashboard e forma de exibição de img

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb+srv://gostack:gostack@gostack-m9tll.mongodb.net/semana
 // req.body = Acessar corpo da requisação( para criação, edição)
 app.use(cors())
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
