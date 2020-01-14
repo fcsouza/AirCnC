fe0642c90e73b0dfc0ed244db1b9129826511d5f
fcsouza <fcsouza18@hotmail.com>
2020-01-14 20:08:10 -0300

    Finalização do dashboard e path

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors') ;
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
