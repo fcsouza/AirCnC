5b9dd5dd0a08bf43136bd99ad13748165ffdd648
fcsouza <fcsouza18@hotmail.com>
2020-01-15 23:12:04 -0300

    Criação das pages e rotas

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Routes from './src/routes';
+
 export default function App() {
   return (
     <View style={styles.container}>
