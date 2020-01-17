060540ba9fe6de350e34042287956a7b9381f220
fcsouza <fcsouza18@hotmail.com>
2020-01-16 22:16:54 -0300

    Inicio da tela de login

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,6 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function Login(){
-    return <View />
+    return (
+    <View style={styles.container}>
+        <Image source={logo} />
+
+        <Text style={styles.label}>SEU E-MAIL *</Text>
+    </View>
+    );
 } 
+
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+});
